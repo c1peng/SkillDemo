19a981cd7d135253be1153ff97ed3ad8dee00488
Chuan Peng <c1peng@ucsd.edu>
2022-02-04 15:56:53 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,6 +5,6 @@ public class SkillDemoTest {
     
     @Test
     public void testminus() {
-        assertEquals(2, SkillDemo.minus(4, 3)); // it supposed to be 5-3
+        assertEquals(2, SkillDemo.minus(5, 3));
     }
 }
